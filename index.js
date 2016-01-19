bddacddfbf5450ca7649cc475626f846cf8d4add
Forbes Lindesay <forbes@lindesay.co.uk>
2016-01-19 19:31:31 +0000

    Add babel-plugin-transform-runtime

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ module.exports = {
     require("babel-plugin-transform-react-display-name"),
     require("babel-plugin-transform-react-display-name"),
     require("babel-plugin-transform-react-jsx"),
-    require("babel-plugin-transform-strict-mode")
+    require("babel-plugin-transform-strict-mode"),
+
+    require("babel-plugin-transform-runtime")
   ]
 };
